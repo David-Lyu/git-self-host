c57654cd908877c177e405c943ff229c2c5a3ae1
David-Lyu <lyu.david@yahoo.com>
2024-07-26 23:41:59 -0700

    finished init script to work with docker file

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -2,7 +2,7 @@
 
 OS=""
 # Check if it is from docker container
-if [ DOCKER_ENV != "DOCKER" ]; then
+if [[ $DOCKER_ENV != "DOCKER" ]]; then
     # Check if it is a linux distro, and if it is debian
     if [ -f /etc/os-release ]; then
         OS=$(. /etc/os-release; echo "$NAME")
@@ -16,10 +16,18 @@ if [ DOCKER_ENV != "DOCKER" ]; then
     fi
 fi
 
-echo $OS
+#Install dependencies if needed
+# Need to check for dependencies
+if [[ $OS == "Debian GNU/Linux" ]]; then
+    apt install git;
+elif [[ DOCKER_ENV == "DOCKER" ]]; then
+    apk add git;
+fi
 
-
-# Install git server
-# Run Go server? Here?
-# We need atleast cmake file here to read main?
-#
+#Run the go backend
+if [[ DOCKER_ENV == "DOCKER" ]]; then
+    ./main
+else
+    #Grabs path of script and goes back a level where main should live
+    $(dirname $0)/../main
+fi
