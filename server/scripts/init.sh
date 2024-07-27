78667f162a3181a84d5e37a943128a8b3c0fa26f
David-Lyu <lyu.david@yahoo.com>
2024-07-26 21:23:10 -0700

    addedd docker env and moving to work in init shell on server

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-#Check os system
-cat /etc/os-release;
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-else
-    echo "We do not support that os";
+OS=""
+# Check if it is from docker container
+if [ DOCKER_ENV != "DOCKER" ]; then
+    # Check if it is a linux distro, and if it is debian
+    if [[ ! -f /etc/os-release ]] || [[ $(grep '^(NAME)=' /etc/os-release) == "Debian GNU/Linux" ]]; then
+        echo "We do not support that os";
+        exit 1;
+    else
+        OS=$(grep '^(NAME)=' /etc/os-release)
+        echo $OS
+        echo $(grep '^(NAME)=' /etc/os-release)
+    fi
 fi
+
+echo $OS
+
 
 # Install git server
 # Run Go server? Here?
