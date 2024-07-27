9e098d0c4f93fca054270cc8f2d09bf39aec25d3
David-Lyu <lyu.david@yahoo.com>
2024-07-26 22:13:42 -0700

    updated script to close if

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -5,10 +5,11 @@ OS=""
 if [ DOCKER_ENV != "DOCKER" ]; then
     # Check if it is a linux distro, and if it is debian
     if [ -f /etc/os-release ]; then
-        OS=$(. /etc/os-release; echo "$NAME")
+        OS=$(cat /etc/os-release | grep "^(NAME)=")
         if [ $OS != "Debian GNU/Linux" ]; then
             echo "We do not support the linux distro";
             exit 1;
+        fi
     else
         echo "We do not support that os";
         exit 1;
