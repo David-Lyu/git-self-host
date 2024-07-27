bf33604dcb89e3cfdd01a6e74e3032c4c385e52c
David-Lyu <lyu.david@yahoo.com>
2024-07-26 22:00:25 -0700

    grabed os out

--- server/scripts/init.sh
+++ server/scripts/init.sh
@@ -4,13 +4,14 @@ OS=""
 # Check if it is from docker container
 if [ DOCKER_ENV != "DOCKER" ]; then
     # Check if it is a linux distro, and if it is debian
-    if [[ ! -f /etc/os-release ]] || [[ $(grep '^(NAME)=' /etc/os-release) == "Debian GNU/Linux" ]]; then
+    if [ -f /etc/os-release ]; then
+        OS=$(. /etc/os-release; echo "$NAME")
+        if [ $OS != "Debian GNU/Linux" ]; then
+            echo "We do not support the linux distro";
+            exit 1;
+    else
         echo "We do not support that os";
         exit 1;
-    else
-        OS=$(grep '^(NAME)=' /etc/os-release)
-        echo $OS
-        echo $(grep '^(NAME)=' /etc/os-release)
     fi
 fi
 
